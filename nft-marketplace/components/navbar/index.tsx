5254d306ef936e1e54644e965560d96e23cb6b1b
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-11-20 07:09:09 +0500

    Added Profile Link

--- nft-marketplace/components/navbar/index.tsx
+++ nft-marketplace/components/navbar/index.tsx
@@ -85,15 +85,15 @@ export default function Navbar() {
                   <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
                       {({ active }) => (
-                        <a
-                          href="#"
+                        <Link
+                          href="/profile"
                           className={classNames(
                             active ? "bg-gray-100" : "",
                             "block px-4 py-2 text-sm text-gray-700"
                           )}
                         >
                           Your Profile
-                        </a>
+                        </Link>
                       )}
                     </Menu.Item>
                     <Menu.Item>
---
+++ nft-marketplace/pages/profile/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../../components";
+
+const Profile: NextPage = () => {
+  return <BaseLayout>My Profile</BaseLayout>;
+};
+
+export default Profile;
